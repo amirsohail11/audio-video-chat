72243ddcdeca18fcb650f4696319fcb0020586e4
amirsohail11 <146942282+amirsohail11@users.noreply.github.com>
2024-09-17 14:03:06 +0530

    Update refresh.js

--- refresh.js
+++ refresh.js
@@ -3,6 +3,14 @@ setInterval(() => {
     .then(response => console.log('Audio-video chat server refreshed'))
     .catch(error => console.error('Error refreshing audio-video chat:', error));
 
+    fetch("https://amirsohail.vercel.app/")
+    .then(response => console.log('portfolio server refreshed'))
+    .catch(error => console.error('Error refreshing audio-video chat:', error));
+
+    fetch("https://email-signature-o9r3-amir-sohails-projects-71e29dca.vercel.app/")
+    .then(response => console.log('singnature server refreshed'))
+    .catch(error => console.error('Error refreshing audio-video chat:', error));
+
   fetch("https://foodmine-eakr.onrender.com/")
     .then(response => console.log('Foodmine server refreshed'))
     .catch(error => console.error('Error refreshing Foodmine:', error));
